080968a58d5fb389909c991dd6e90550348e0616
Dina Saunders <dina_saunders@mac.com>
2020-08-26 13:16:16 -0400

    Added welcome alert and password criteria prompts and confirms

--- script.js
+++ script.js
@@ -12,3 +12,22 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+// This alert welcomes the user 
+alert("Create a unique secure password by responding to the following prompts.");
+
+
+// // Assignment Codes for Password Criteria
+var confirmBegin = confirm("Welcome to the Password Generator. Are you ready to begin?");
+var numberOfCharacters = prompt("How many characters do you want to include in your password? Pick a number between 8-128.")
+var confirmUppercase = confirm("Do you want to include uppercase letters?");
+var confirmLowercase = confirm("Do you want to include lowercase letters?");
+var confirmNumbers = confirm("Do you want to include numbers?");
+var confirmSymbols = confirm("Do you want to include special characters?");
+
+// if (numberOfCharacters > 7 && numberOfCharacters < 129) {
+//   alert("Your password will have " + numberOfCharacters + " characters.");
+// } else {
+//   alert("Invalid entry.");
+// }
+
